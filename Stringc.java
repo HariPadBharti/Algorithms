c3937513aaf921dc9a1a276ac30c72f3a6b042b7
HARI PAD BHARTI <haripadbharti@gmail.com>
2017-04-20 22:58:24 -0400

    Update Stringc.java

--- Stringc.java
+++ Stringc.java
@@ -1,35 +1,35 @@
-// String 
+// // String 
 
-Alice wrote a sequence of words in CamelCase as a string of letters, , having the following properties:
+// Alice wrote a sequence of words in CamelCase as a string of letters, , having the following properties:
 
-It is a concatenation of one or more words consisting of English letters.
-All letters in the first word are lowercase.
-For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
-Given , print the number of words in  on a new line.
+// It is a concatenation of one or more words consisting of English letters.
+// All letters in the first word are lowercase.
+// For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+// Given , print the number of words in  on a new line.
 
-Input Format
+// Input Format
 
-A single line containing string .
+// A single line containing string .
 
-Constraints
-1 <= |s| <= 1000000
+// Constraints
+// 1 <= |s| <= 1000000
 
-Output Format
+// Output Format
 
-Print the number of words in string .
+// Print the number of words in string .
 
-Sample Input
+// Sample Input
 
-saveChangesInTheEditor
-Sample Output
+// saveChangesInTheEditor
+// Sample Output
 
-5
-Explanation
+// 5
+// Explanation
 
-String  contains five words:
+// String  contains five words:
 
-save
-Changes
-In
-The
-Editor
+// save
+// Changes
+// In
+// The
+// Editor
