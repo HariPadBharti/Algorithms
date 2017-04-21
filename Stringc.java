df86627c0829f62b448bea3968e23baaea13ad40
HARI PAD BHARTI <haripadbharti@gmail.com>
2017-04-20 22:59:10 -0400

    Update Stringc.java

--- Stringc.java
+++ Stringc.java
@@ -33,3 +33,37 @@
 // In
 // The
 // Editor
+
+
+//Code : 
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Stringc {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+   
+        int count = 1;
+        int a=s.length();
+        if(a>=1 && a<=100000)
+        for(int i=1; i<a; i++)
+            {
+            char a_char = s.charAt(i);
+            
+            if((int)a_char>=65 && (int)a_char<=90)
+            count++;
+            
+            
+            
+        }
+        System.out.println(count);
+        
+    }
+}
